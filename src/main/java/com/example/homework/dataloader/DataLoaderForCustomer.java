640030e91421c96caae8d27eefcd751a7a483446
Aditya <adityakolhapure7@gmail.com>
2022-03-31 10:50:28 -0400

    added data loaders

--- src/main/java/com/example/homework/dataloader/DataLoaderForCustomer.java
+++ src/main/java/com/example/homework/dataloader/DataLoaderForCustomer.java
@@ -11,7 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 
-//@Component
+@Component
 public class DataLoaderForCustomer implements CommandLineRunner{
 
     private CustomerRepository customerRepository;
--- src/main/java/com/example/homework/dataloader/DataLoaderForProduct.java
+++ src/main/java/com/example/homework/dataloader/DataLoaderForProduct.java
@@ -11,7 +11,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 
-//@Component
+@Component
 public class DataLoaderForProduct implements CommandLineRunner{
 
     private ProductRepository productRepository;
