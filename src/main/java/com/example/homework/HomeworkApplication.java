56749bccd11833ef4852a8be0ef0a99a9f7828a2
Adi K <46882313+RAVAN55@users.noreply.github.com>
2022-04-13 03:48:38 -0400

    Update HomeworkApplication.java

--- src/main/java/com/example/homework/HomeworkApplication.java
+++ src/main/java/com/example/homework/HomeworkApplication.java
@@ -9,8 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-//@ComponentScan(basePackages = {"com.example.homework"})
-//@EnableJpaRepositories("")
 @EntityScan
 public class HomeworkApplication{
 
